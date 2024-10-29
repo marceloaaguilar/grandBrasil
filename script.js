3165e28aebc60ce953cb4ecffca6bdbc0ab1ad40
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2024-10-28 23:33:33 -0300

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const swiper = new Swiper(".mySwiper", {
+      loop: true,
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+      },
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+      },
+  });
+});
