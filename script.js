0779dcaf13dea729712a55b929384dd6a913d043
Marcelo D'almeida <marcelodalmeida42@gmail.com>
2025-01-19 21:09:15 -0300

    init

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const swiper = new Swiper(".mySwiper", {
-      loop: true,
-      pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-      },
-      navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-      },
-  });
-
-    var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+window.onload = function() {
+    var myModal = new bootstrap.Modal(document.getElementById('modalAccess'));
     myModal.show();
 
-});
+    var swiper = new Swiper(".header", {
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+      });
+}
+
+document.getElementById('btnNo').onclick = function() {
+    document.querySelector('.error-message').style.display = 'block';
+    document.getElementById('contentModal').style.display = 'none';
+    document.querySelector('.footer-modal-custom').style.display = 'none';
+}
 
 
 
